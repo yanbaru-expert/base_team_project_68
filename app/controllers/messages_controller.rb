dab086deaa1c6333ed01f1274699d81b3acf04c3
boy-com777 <hiroyukiogasawara@ogasawarahiroyukinoMacBook-Pro.local>
2021-05-11 22:08:00 +0900

    add new-action, create-action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,18 @@
 class MessagesController < ApplicationController
+
+  # newアクション
+  def new
+    @message = Message.new
+  end
+
+  # createアクション
+  def create
+    Message.create(message_params)
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:title, :contents)
+  end
 end
